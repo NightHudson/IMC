e5142d704ebd3db5afc730e67c4c4adaa3cc4772
Hudson Bezerra <hudsonbezerra77@gmail.com>
2025-04-25 11:07:16 -0300

    add toFixed(2)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calcularIMC() {
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
 
-    const imc = (peso / (altura * altura)).toFixed(1);
+    const imc = (peso / (altura * altura)).toFixed(2);
 
     document.querySelector("p").innerHTML = imc;
     }
